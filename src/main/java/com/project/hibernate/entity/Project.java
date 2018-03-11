b1ef47f4b889f89ebff0338d603429412687542e
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-11 19:34:59 +0200

    add project model

---
+++ src/main/java/com/project/hibernate/entity/Project.java
@@ -0,0 +1,35 @@
+package com.project.hibernate.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "project")
+@Data
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String name;
+
+    private String content;
+
+
+    private Department department;
+
+    @OneToOne
+    private ProjectManager projectManager;
+
+    @ManyToMany
+    @JoinTable(name = "user_project", joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private Set<User> member = new HashSet<>();
+
+
+
+}
