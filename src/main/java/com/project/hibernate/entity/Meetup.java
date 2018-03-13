ac6eaf54fe18c16eb09e98e756528695e0603668
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-13 22:59:59 +0200

    update meetup entity

--- src/main/java/com/project/hibernate/entity/Meetup.java
+++ src/main/java/com/project/hibernate/entity/Meetup.java
@@ -1,4 +1,45 @@
 package com.project.hibernate.entity;
 
+import lombok.Data;
+import org.hibernate.validator.constraints.NotEmpty;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "meetup ")
 public class Meetup {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "meetup_id")
+    private Integer id;
+
+    @NotNull
+    private String location;
+
+    @DateTimeFormat
+    private LocalDate date;
+
+    @DateTimeFormat
+    private LocalTime time;
+
+
+    @ManyToOne
+    private User organizer;
+
+    @ManyToMany
+    @JoinTable(name = "user_meetups_attending", joinColumns = @JoinColumn(name = "meetup_id"), inverseJoinColumns = @JoinColumn(name = "user_id"))
+    private List<User> attendees = new ArrayList<>();
+
+    @Column(name = "description")
+    private String description;
+
 }
