57406db5d0238bbb1cca2268c26d10df9e696bd5
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-17 11:00:18 +0200

    fix configuration beans of hibernate

--- src/main/java/com/project/hibernate/config/hibernate/HibernateConfiguration.java
+++ src/main/java/com/project/hibernate/config/hibernate/HibernateConfiguration.java
@@ -1,6 +1,5 @@
 package com.project.hibernate.config.hibernate;
 
-
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,16 +8,18 @@ import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.sql.DataSource;
 
+
 @Configuration
 @EntityScan("com.project.hibernate.entity")
 @EnableTransactionManagement
 @PropertySource("classpath:application.properties")
-public class AppConfig {
+public class HibernateConfiguration {
 
     // CONFIG data source
     @Value("${spring.datasource.driverClassName}") String
@@ -46,6 +47,17 @@ public class AppConfig {
         sessionBuilder.scanPackages("com.project.hibernate.entity");
         return sessionBuilder.buildSessionFactory();
     }
+
+    // HIBERNATE TRANSACTION MANAGER
+
+    @Bean(name = "transactionManager")
+    public HibernateTransactionManager getTransactionManager(
+            SessionFactory sessionFactory) {
+        HibernateTransactionManager transactionManager = new
+                HibernateTransactionManager(sessionFactory);
+        return transactionManager;
+    }
+
 
 
 
