d3fc2946fe41be9212cb3c0980250ed76e0aaa0b
hiennguyenbookygroupfi <hienminhnguyen711@gmail.com>
2018-03-17 20:02:04 +0200

    update simple cron job run at 12pm everyday

--- src/main/java/com/project/hibernate/scheduling/ScheduledTasks.java
+++ src/main/java/com/project/hibernate/scheduling/ScheduledTasks.java
@@ -17,7 +17,16 @@ public class ScheduledTasks {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
 
     @Scheduled(fixedRate = 5000)
+//    @Scheduled(cron = "[Seconds] [Minutes] [Hours] [Day of month] [Month] [Day of week] [Year]")
     public void reportCurrentTime() {
         log.info("The time is now {}", dateFormat.format(new Date()));
     }
+
+
+    // fire at 12 pm everyday
+    @Scheduled(cron = "0 0 12 * * ?")
+    public void report12pmeveryday(){
+        log.info("12 pm everyday The time is now {}", dateFormat.format(new Date()));
+    }
+
 }
